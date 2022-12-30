7703fe9e694fa43d7f0a4ac7814af7cdbaf5acce
achyutsharan3267 <achyutsharan95@gmail.com>
2022-12-31 03:02:19 +0530

    header,banner UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MaintenancePageComponent } from './maintenance/maintenance-page/maintenance-page.component';
+import { HeaderComponent } from './container/header/header.component';
+import { HeroSectionComponent } from './content/hero-section/hero-section.component';
+import { AboutUsComponent } from './content/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MaintenancePageComponent
+    MaintenancePageComponent,
+    HeaderComponent,
+    HeroSectionComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule
